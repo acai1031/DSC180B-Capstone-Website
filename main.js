31ff58e9171d7ba266544067f48c9e3cd1cf2458
acai1031 <cys021031@163.com>
2025-02-23 11:47:01 -0800

    add logo

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
 // scroll down change navbar from transparent to light
 window.addEventListener('scroll', function() {
     var navbar = document.getElementById('introNavbar');
+    var logo = document.getElementById('hdsi-logo');
+    
     if (window.scrollY > document.querySelector('.full-page-image').offsetHeight) {
         navbar.classList.remove('bg-transparent', 'navbar-light-text');
         navbar.classList.add('bg-light', 'with-box-shadow');
+        logo.src = "images/hdsi-blue-gold.png"; 
     } else {
         navbar.classList.remove('bg-light', 'with-box-shadow');
         navbar.classList.add('bg-transparent', 'navbar-light-text');
+        logo.src = "images/hdsi-white.png"; 
     }
 });
